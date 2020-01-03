394e0407ed0ca9ea2c1e353c85af93282dc4499d
edu <evela@minsait.com>
2020-01-03 11:59:01 +0100

    apply prettier to project

--- src/app/map.service.ts
+++ src/app/map.service.ts
@@ -2,22 +2,23 @@ import { Injectable } from '@angular/core';
 import * as L from 'leaflet';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class MapService {
-  private map = L.map('map', {
-    center: [ 39.8282, -98.5795 ],
-    zoom: 3
-  });
+    private map = L.map('map', {
+        center: [39.8282, -98.5795],
+        zoom: 3,
+    });
 
-  tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19,
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-  }).addTo(this.map);
+    tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution:
+            '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(this.map);
 
-  constructor() { }
+    constructor() {}
 
-  getMap() {
-    return this.map;
-  }
+    getMap() {
+        return this.map;
+    }
 }
