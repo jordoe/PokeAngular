b099d2a4541b8c9ed97609237fab3e0fda4f08fe
Jorge D <jdomingueze@indra.es>
2020-01-03 08:09:52 +0100

    add map component and functionality

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -15,10 +15,21 @@ export class MapComponent implements AfterViewInit {
     this.initMap()
   }
 
+  private myIcon = L.icon({
+    iconUrl: "https://freepngimg.com/thumb/pokemon/6-2-pokemon-png-hd.png",
+    iconSize: [38, 38]
+  })
+
+  private randomLocation = [Math.floor(Math.random()*(90-(-90)+1)+(-90)), Math.floor(Math.random()*(180-(-180)+1)+(-180))]
+
   private initMap(): void {
     this.map = L.map('map', {
-      center: [ 39.8282, -98.5795 ],
-      zoom: 3
+      center: this.randomLocation,
+      zoom: 3,
+      attributionControl: false,
+      maxBounds: L.latLngBounds(L.latLng(-90, -200), L.latLng(90, 200)),
+      maxBoundsViscosity: 1.0
+
     })
     const tiles = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
@@ -26,5 +37,6 @@ export class MapComponent implements AfterViewInit {
       maxZoom: 19
     })
     tiles.addTo(this.map)
+    L.marker(this.randomLocation, {icon: this.myIcon}).addTo(this.map)
   }
 }
