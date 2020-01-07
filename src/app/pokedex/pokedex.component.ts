e2e96fff46d229ad438eaa10f84fb570fdf20760
ruben <rmcantero@minsait.com>
2020-01-07 08:39:04 +0100

    resolve merge conflit

--- src/app/pokedex/pokedex.component.ts
+++ src/app/pokedex/pokedex.component.ts
@@ -11,35 +11,20 @@ export class PokedexComponent implements OnInit {
     pokemon = {};
     ability = {};
 
-<<<<<<< HEAD
-    pokemonList: object[];
-    pokemonSelected;
-    displayNone = false;
-=======
   pokemonList: object[];
   pokemonSelected: any = undefined;
 
   showList = true;
   showDetail = false;
 
->>>>>>> feature-detail-style
 
     constructor(private pokedexService: PokedexService) {}
 
-<<<<<<< HEAD
     ngOnInit() {
         this.pokedexService.getPokemonList().then(response => {
             this.pokemonList = response;
             this.pokemonSelected = response[0];
         });
-    }
-
-    pokemonSelect(pokemon) {
-        this.pokedexService
-            .getPokemonByName(pokemon.name)
-            .subscribe(pokeFull => {
-                this.pokemonSelected = pokeFull;
-            });
     }
 
     typeSelect(type) {
@@ -48,32 +33,15 @@ export class PokedexComponent implements OnInit {
             this.pokemonSelected = response[0];
         });
     }
-=======
-  ngOnInit() {
-	this.pokedexService.getPokemonList().then((response) =>{
-		this.pokemonList = response
-	})
-  }
+    pokemonSelect(pokemon){
+      this.pokedexService.getPokemonByName(pokemon.name).subscribe((pokeFull) => {
+        this.pokemonSelected = pokeFull;
+        console.log(this.pokemonSelected);
+      })
+    }
 
-
-  pokemonSelect(pokemon){
-  	this.pokedexService.getPokemonByName(pokemon.name).subscribe((pokeFull) => {
-      this.pokemonSelected = pokeFull;
-      console.log(this.pokemonSelected);
-  	})
-  }
-
-  typeSelect(type) {
-    this.pokedexService.getPokemonByType(type).then((response) => {
-      this.pokemonList = response;
-      this.pokemonSelected = response[0];
-    });
-}
-
-  changeView() {
-    this.showList = !this.showList;
-    this.showDetail = !this.showDetail;
-  }
-
->>>>>>> feature-detail-style
+    changeView() {
+      this.showList = !this.showList;
+      this.showDetail = !this.showDetail;
+    }
 }
