e668aa114d8783e9a256bcfdababed23e2ffcb13
edu <evela@minsait.com>
2020-01-07 10:24:24 +0100

    lint commit

--- src/app/pokedex-detail/pokedex-detail.component.ts
+++ src/app/pokedex-detail/pokedex-detail.component.ts
@@ -21,6 +21,8 @@ export class PokedexDetailComponent implements OnInit {
     }
 
     changeView() {
-        if (this.pokemonSelected !== undefined) this.changeViewEvent.emit();
+        if (this.pokemonSelected !== undefined){ 
+            this.changeViewEvent.emit();
+        }
     }
 }
