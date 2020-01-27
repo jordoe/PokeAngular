50bce29e9e65f892bc3c4702281437494e0ef603
Jorge D <jdomingueze@minsait.com>
2020-01-27 12:49:24 +0100

    try fix 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -62,7 +62,7 @@ import { PokedexTopPokemonComponent } from './Components/pokedex-top-pokemon/pok
                 useFactory: (http: HttpClient) => {
                     return new TranslateHttpLoader(
                         http,
-                        './PokeAngular/assets/i18n/',
+                        '/public/test',
                         '.json'
                     );
                 },
